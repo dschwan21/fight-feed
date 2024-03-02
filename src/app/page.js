d151070662c5a7562b95a51e326171ba9554cace
Derek Schwantner <derek@Dereks-Mac-mini.local>
2024-03-01 21:44:38 -0500

    frontend is talking to db and displaying users

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,26 @@
 import { options } from "./api/auth/[...nextauth]/options"
 import { getServerSession } from "next-auth/next"
+import { createClient } from "@supabase/supabase-js"
 import UserCard from "./components/UserCard"
 
 export default async function Home() {
   const session = await getServerSession(options)
-
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  const { data: users } = await supabase.from('users').select('*')
+  console.log('hi', supabaseUrl, supabaseKey, users)
   return (
     <>
       {session ? (
-        <UserCard user={session?.user} pagetype={"Home"} />
+        <div>
+          <UserCard user={session?.user} pagetype={"Home"} />
+          <ul>
+            {users.map((user, index) => (
+              <li key={index}>{user.username}</li>
+            ))}
+          </ul>
+        </div>
       ) : (
         <h1 className="text-5xl">You Shall Not Pass!</h1>
       )}
