b372df25d488835cc2efd9b9dd4f86789cfbd4bd
Derek Schwantner <derek@Dereks-Mac-mini.local>
2024-03-06 21:27:54 -0500

    removed supabase calls

--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,28 @@ import { options } from "./api/auth/[...nextauth]/options"
 import { getServerSession } from "next-auth/next"
 import { createClient } from "@supabase/supabase-js"
 import UserCard from "./components/UserCard"
+import { PrismaClient } from '@prisma/client';
 
 export default async function Home() {
   const session = await getServerSession(options)
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-  const supabase = createClient(supabaseUrl, supabaseKey);
-  const { data: users } = await supabase.from('users').select('*')
-  console.log('hi', supabaseUrl, supabaseKey, users)
+
+
+  const prisma = new PrismaClient()
+
+  console.log('users', users)
+  async function handler(req, res) {
+  const users = await prisma.User.findMany();
+  res.json(users);
+}
   return (
     <>
       {session ? (
         <div>
           <UserCard user={session?.user} pagetype={"Home"} />
           <ul>
-            {users.map((user, index) => (
+            {users?.map((user, index) => (
               <li key={index}>{user.username}</li>
             ))}
           </ul>
