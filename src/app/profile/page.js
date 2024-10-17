47a87e8e0c1782a1d0f6b315525cd3e3b79a87d1
Derek Schwantner <derek@Dereks-Mac-mini.local>
2024-10-16 20:44:56 -0400

    profile page infrastructure added. avatar, bio, buttons

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import ProfileHeader from './components/ProfileHeader';
+import ProfileStats from './components/ProfileStats';
+import ProfileBio from './components/ProfileBio';
+import ProfileActionButtons from './components/ProfileActionButtons';
+import styles from './ProfilePage.module.css';
 
-export default async function Profile() {
+export default function Profile() {
   return (
-    <div>
-      <h1>welcome to the profile</h1>
+    <div className={styles.profileContainer}>
+      <ProfileHeader />
+      <ProfileActionButtons />
+      <ProfileStats />
+      <ProfileBio />
     </div>
   );
 }
